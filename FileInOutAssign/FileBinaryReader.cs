0c8a44ad99e4c9b49654fb7d297ff079a6c22243
Lavanya.Allu <allu.lavanya@Zensar.com>
2021-01-27 22:02:35 +0530

    Add project files.

---
+++ FileInOutAssign/FileBinaryReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileInOutAssign
+{
+    /*C# BinaryReader
+    C# BinaryReader class is used to read binary information from stream. It is found in System.IO namespace. It also supports reading string in specific encoding.
+
+    C# BinaryReader Example
+    Let's see the simple example of BinaryReader class which reads data from dat file.C# BinaryReader*/
+    class FileBinaryReader
+    {
+        static void Main(string[] args)
+        {
+            WriteBinaryFile();
+            ReadBinaryFile();
+            Console.ReadKey();
+        }
+        static void WriteBinaryFile()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("e:\\binaryfile.dat", FileMode.Create)))
+            {
+
+                writer.Write(12.5);
+                writer.Write("this is string data");
+                writer.Write(true);
+            }
+        }
+        static void ReadBinaryFile()
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open("e:\\binaryfile.dat", FileMode.Open)))
+            {
+                Console.WriteLine("Double Value : " + reader.ReadDouble());
+                Console.WriteLine("String Value : " + reader.ReadString());
+                Console.WriteLine("Boolean Value : " + reader.ReadBoolean());
+            }
+        }
+    }
+}
+
---
+++ FileInOutAssign/FileBinaryWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileInOutAssign
+{
+   /* C# BinaryWriter
+    C# BinaryWriter class is used to write binary information into stream. It is found in System.IO namespace. It also supports writing string in specific encoding.
+
+    C# BinaryWriter Example
+    Let's see the simple example of BinaryWriter class which writes data into dat file.*/
+    class FileBinaryWriter
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "e:\\binaryfile.dat";
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                writer.Write(2.5);
+                writer.Write("this is string data");
+                writer.Write(true);
+            }
+            Console.WriteLine("Data written successfully...");
+        }
+    }
+}
